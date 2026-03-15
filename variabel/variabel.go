88319ed237a4de150c3aa851988bd976856ddf89
dickyyahya <dickyyahya25@gmail.com>
2026-03-15 14:28:46 +0700

    Belajar Variabel

---
+++ variabel/variabel.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nama string = "yahya"
+	var umur int = 20
+
+	city := "Brebes"
+	year := 2026
+
+	fmt.Println("Nama : ", nama)
+	fmt.Println("Umur : ", umur)
+	fmt.Println("Kota : ", city)
+	fmt.Println("Tahun : ", year)
+
+}
