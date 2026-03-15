f0df297f8a26fea184f87ce9edd2f39db5c48efb
dickyyahya <dickyyahya25@gmail.com>
2026-03-15 16:29:40 +0700

    belajar tipe data floating point

---
+++ tipe-data-floatingpoint/floating-point.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var f32 float32 = 33.1415926535
+	var f64 float64 = 3.14159265358979323846
+
+	fmt.Println("Nilai float32:", f32)
+	fmt.Println("Nilai float32:", f64)
+}
