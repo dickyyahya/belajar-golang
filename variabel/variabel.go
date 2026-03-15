af349cd99df15c7a7254e08691ee03a74c44ff7f
dickyyahya <dickyyahya25@gmail.com>
2026-03-15 14:39:15 +0700

    Belajar konstanta

--- variabel/variabel.go
+++ variabel/variabel.go
@@ -3,15 +3,21 @@ package main
 import "fmt"
 
 func main() {
+	// variabel itu ibarat gelas dan nilai sebagai isinya atau sama seperti air
 	var nama string = "yahya"
 	var umur int = 20
 
 	city := "Brebes"
 	year := 2026
+	// sedangkan konstanta itu seperti variabel tapi nilainya tidak bisa diubah contoh pi jumlah hari
+	const pi = 3.14
+	const week = 7
 
 	fmt.Println("Nama : ", nama)
 	fmt.Println("Umur : ", umur)
 	fmt.Println("Kota : ", city)
 	fmt.Println("Tahun : ", year)
+	fmt.Println("Pi :", pi)
+	fmt.Println("Week :", week)
 
 }
