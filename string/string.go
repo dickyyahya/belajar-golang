26a45cbae3e2a2d9abfe37598eb54bc32664ffb5
dickyyahya <dickyyahya25@gmail.com>
2026-03-15 21:41:37 +0700

    belajar string part 1

---
+++ string/string.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	nama := "yahya"
+	pesan := "woi tobat sialan"
+
+	paragraf := `hidup tenang dan sejahtera lalu 
+	mati masuk surga`
+
+	fmt.Println("Nama :", nama)
+	fmt.Println("Pesan :", pesan)
+	fmt.Println("Paragraf : ", paragraf)
+}
